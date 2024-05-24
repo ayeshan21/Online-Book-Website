5d5738f14833800635f7558fe3b09d841f7b6e09
ayeshan21 <ayeshasiddiqua.iubat@gmail.com>
2024-05-24 09:34:28 +0600

    Add project files.

---
+++ Online-Book-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Book_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
